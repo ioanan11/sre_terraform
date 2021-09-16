ee1c7d7119ff0a9e0e5a40b168fdfc02ea416fa5
Ioana <inicolae@spartaglobal.com>
2021-09-16 16:00:26 +0100

    code update

--- network.tf
+++ network.tf
@@ -28,7 +28,7 @@ resource "aws_route_table" "sre_ioana_RT_terraform" {
 resource "aws_network_acl" "sre_ioana_public_nacl_terra" {
 	vpc_id = aws_vpc.sre_ioana_vpc_terraform.id
 
-	# allow HTTP from all
+	
 	ingress {
 		protocol = "tcp"
 		rule_no = 100
@@ -38,7 +38,7 @@ resource "aws_network_acl" "sre_ioana_public_nacl_terra" {
 		to_port = 80
 	}
 
-	# allow HTTPS from all
+	
 	ingress {
 		protocol = "tcp"
 		rule_no = 110
@@ -48,7 +48,7 @@ resource "aws_network_acl" "sre_ioana_public_nacl_terra" {
 		to_port = 443
 	}
 
-	# allow SSH from home
+	
 	ingress {
 		protocol = "tcp"
 		rule_no = 120
@@ -58,7 +58,7 @@ resource "aws_network_acl" "sre_ioana_public_nacl_terra" {
 		to_port = 22
 	}
 
-	# allow ephemeral from all
+	
 	ingress {
 		protocol = "tcp"
 		rule_no = 130
@@ -68,7 +68,7 @@ resource "aws_network_acl" "sre_ioana_public_nacl_terra" {
 		to_port = 65535
 	}
 	
-	# allow HTTP to all
+	
 	egress {
 		protocol = "tcp"
 		rule_no = 100
@@ -78,7 +78,7 @@ resource "aws_network_acl" "sre_ioana_public_nacl_terra" {
 		to_port = 80
 	}
 
-	# allow HTTPS to all
+	
 	egress {
 		protocol = "tcp"
 		rule_no = 110
@@ -88,7 +88,7 @@ resource "aws_network_acl" "sre_ioana_public_nacl_terra" {
 		to_port = 443
 	}
 
-	# allow SSH to home
+	
 	egress {
 		protocol = "tcp"
 		rule_no = 120
@@ -98,7 +98,7 @@ resource "aws_network_acl" "sre_ioana_public_nacl_terra" {
 		to_port = 22
 	}
 
-	# allow ephemeral to all
+	
 	egress {
 		protocol = "tcp"
 		rule_no = 130
@@ -108,7 +108,7 @@ resource "aws_network_acl" "sre_ioana_public_nacl_terra" {
 		to_port = 65535
 	}
 
-	# allow 27017 to private subnet
+	
 	egress {
 		protocol = "tcp"
 		rule_no = 140
